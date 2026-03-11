386394677322d4de19fce7caaf5a936dbcd1fea7
24-0339C <24-0339C@CCL1-04>
2026-03-11 13:32:42 +0800

    finished all pages

--- MVVMLoginApp/ViewModels/MainViewModel.cs
+++ MVVMLoginApp/ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using MVVMLoginApp.Models;
-
 namespace MVVMLoginApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
@@ -7,25 +6,30 @@ namespace MVVMLoginApp.ViewModels
         private readonly AuthService _authService;
         private User? _currentUser;
         private BaseViewModel? _currentView;
+
         public BaseViewModel? CurrentView
         {
             get => _currentView;
             set => SetProperty(ref _currentView, value);
         }
+
         public MainViewModel()
         {
             _authService = new AuthService();
             NavigateToLogin();
         }
+
         private void NavigateToLogin()
         {
             CurrentView = new LoginViewModel(_authService, OnLoginSuccess);
         }
+
         private void OnLoginSuccess(User user)
         {
             _currentUser = user;
             NavigateToCinemaMain();
         }
+
         private void NavigateToCinemaMain()
         {
             CurrentView = new CinemaMainViewModel(NavigateToLogin, NavigateTo);
@@ -33,7 +37,20 @@ namespace MVVMLoginApp.ViewModels
 
         private void NavigateTo(string page)
         {
-            // Pages will be added here as we build them
+            CurrentView = page switch
+            {
+                "ViewAllMovies" => new ViewAllMoviesViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "ViewAllShowings" => new ViewAllShowingsViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "ScheduleaShowing" => new ScheduleaShowingViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "MakeaReservation" => new MakeaReservationViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "ViewAvailableSeats" => new ViewAvailableSeatsViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "ViewAllReservations" => new ViewAllReservationsViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "ViewFirstandLastShowing" => new ViewFirstandLastShowingViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "ViewCinemaRooms" => new ViewCinemaRoomsViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "AddaMovie" => new AddaMovieViewModel(NavigateToCinemaMain, NavigateToLogin),
+                "SaveandExit" => new SaveandExitViewModel(NavigateToCinemaMain, NavigateToLogin),
+                _ => CurrentView
+            };
         }
     }
 }
---
+++ MVVMLoginApp/ViewModels/ViewCinemaRoomsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MVVMLoginApp.Commands;
+using System;
+using System.Windows.Input;
+
+namespace MVVMLoginApp.ViewModels
+{
+    public class ViewCinemaRoomsViewModel : BaseViewModel
+    {
+        private readonly Action _onBackToMainMenu;
+        private readonly Action _onLogout;
+
+        public ICommand BackToMainMenuCommand { get; }
+        public ICommand LogoutCommand { get; }
+
+        public ViewCinemaRoomsViewModel(Action onBackToMainMenu, Action onLogout)
+        {
+            _onBackToMainMenu = onBackToMainMenu;
+            _onLogout = onLogout;
+
+            BackToMainMenuCommand = new RelayCommand(ExecuteBackToMainMenu);
+            LogoutCommand = new RelayCommand(ExecuteLogout);
+        }
+
+        private void ExecuteBackToMainMenu()
+        {
+            _onBackToMainMenu();
+        }
+
+        private void ExecuteLogout()
+        {
+            _onLogout();
+        }
+    }
+}
